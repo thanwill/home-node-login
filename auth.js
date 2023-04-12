8b8d3e86bbc4dbf92cb6b2fdadb34529ed7a47e8
Jonathan Pereira <jonathan14willian@icloud.com>
2023-04-12 12:34:13 -0300

    Crud
    - uso de LocalStorage
    - autenticação
    - criação de usuário
    - delete por ID
    - delete base

--- auth.js
+++ auth.js
@@ -1,37 +1,105 @@
-const usuarios = require('./data');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-async function autentication(user) {
+const LocalStorage = require('node-localstorage').LocalStorage;
+const localStorage = new LocalStorage('./scratch'); // 
 
-    try {
+async function list() {
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    return users.map(u => omitPassword(u));
+}
 
-        const email = user.email.toString();
-        const password = user.password.toString();
+async function create(newUser) {
 
-        let newuser = usuarios.find(usuario => email === usuario.email && password === usuario.password);
+    const nome = newUser.nome.toString();
+    const email = newUser.email.toString();
+    const senha = newUser.senha.toString();
+    const nascimento = newUser.nascimento.toString();
 
-        if (newuser) {
-            const id = newuser.id;
-            const token = jwt.sign({
-                id
-            }, process.env.SECRET, {
-                expiresIn: 300
-            });
+    // Verifica se o usuário já existe
+    const users = JSON.parse(localStorage.getItem('users')) || [];
+    const user = users.find(u => u.email === email);
 
-            return {
-                id : id,
-                auth : true,
-                token : token
-            };
-        } else {
-            throw new Error('Usuário não encontrado');
-        }
-    } catch (error) {
-        console.log(error);
+    if (user) {
+        console.log(`Usuário já existe: ${JSON.stringify(user.nome)}`);
+        return user;
+    } else {
+        // Cria um novo usuário
+        const newUser = {
+            id: users.length + 1,
+            nome,
+            email,
+            senha,
+            nascimento,
+            createdDate: new Date(),
+            modifiedDate: new Date()
+        };
+
+        users.push(newUser);
+        localStorage.setItem('users', JSON.stringify(users));
+        console.log(`Usuário ${newUser.nome} criado com sucesso!`)
+        return list();
     }
 }
 
+function omitPassword(user) {
+    const {
+        password,
+        ...userWithoutPassword
+    } = user;
+    return userWithoutPassword;
+}
+
+async function deleteById(id) {
+    let users = JSON.parse(localStorage.getItem('users'));
+    users = users.filter(u => u.id !== id);
+    localStorage.setItem('users', JSON.stringify(users));
+    return list();
+}
+
+async function autentication(newUser) {
+
+    const {
+        email,
+        senha
+    } = newUser;
+
+    // Verifica se o usuário existe
+    const users = JSON.parse(localStorage.getItem('users'));
+    const user = users.find(u => u.email === email && u.senha === senha);
+
+    if (user === undefined) {
+        console.log(`Usuário não encontrado`);
+        return {
+            auth: false,
+            token: null
+        };
+    } else {
+        const id = user.id;
+        const token = jwt.sign({
+            id
+        }, process.env.SECRET, {
+            expiresIn: 300
+        });
+
+        return {
+            id: id,
+            auth: true,
+            token: token
+        };
+    }
+
+}
+
+async function deleteAll() {
+    localStorage.clear();
+    return list();
+}
+
 module.exports = {
-    autentication
+    autentication,
+    create,
+    list,
+    deleteById,
+    deleteAll
 };
--- server.js
+++ server.js
@@ -3,24 +3,84 @@ const cors = require('cors');
 const auth = require('./auth');
 const port = 3001;
 const app = express();
+const bodyPerser = require('body-parser');
 
 app.use(express.json()); // usado para converter o corpo da solicitação em JSON
 app.use(cors()); // usado para permitir que o servidor seja acessado por outros domínios
+app.use(bodyPerser.json());
 
 app.get('/', (req, res) => {
-    res.render('http://localhost:3000/');
+    return res.json({
+        message: 'Olá mundo!'
+    });
 });
 
-app.get('/',(req, res) => {
-    console.log('Bem-vindo!');
+app.post('/teste', (req, res) => {
+
+    // converte em texto o corpo da requisição
+    const {
+        nome,
+        email
+    } = req.body;
+
+    console.log(nome, email)
+    // retorna o texto convertido
+    return res.send(`<h1>Olá ${nome}! Seu email é ${email}</h1>`);
+})
+
+app.get('/listar', async (req, res) => {
+    const users = await auth.list();
+    return res.json(users);
+})
+
+
+app.post('/cadastrar', async (req, res) => {
+
+    const {
+        nome,
+        email,
+        senha,
+        nascimento
+    } = req.body;
+
+    const user = await auth.create({
+        nome,
+        email,
+        senha,
+        nascimento
+    });
+
+    return res.json(user);
+
+})
+
+app.post('/login', async (req, res) => {
+
+    const {
+        email,
+        senha
+    } = req.body;
+
+    const user = await auth.autentication({
+        email,
+        senha
+    });
+
+    return res.json(user);
 });
 
-app.post('/login', (req,res) => {
-    console.log(req.body);
-    const user = req.body;
-    auth.autentication(user);
-    res.send(user);
+app.delete('/deletar', async (req, res) => {
+    const user = await auth.deleteAll();
+    return res.json(user);
 });
+
+app.delete('/deletar/:id', async (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const user = await auth.deleteById(id);
+    return res.json(user);
+});
+
 
 app.listen(port, () => {
     console.log(`O aplicativo está rodando em http://localhost:${port}`);
